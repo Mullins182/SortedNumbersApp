98f54bd3c79dc4d453a04a0da14a78a4a7aacb37
Mullins182 <Mullins182@t-online.de>
2023-11-19 08:28:46 +0100

    Extended Methods written ...

---
+++ SortedNumbers/ExtMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedNumbers
+{
+    internal static class ExtMethods
+    {
+        public static string Cprint(this string x)              // Extended Method to output strings in console
+        {
+            Console.WriteLine(x);
+            return x;
+        }
+
+        public static int CintPrint(this int x)                 // Extended Method to print any Integer to console and even calculate and print Numbers
+        {                                                       // Example: (15+56-17).CintPrint() ... outputs the calculation result in the console.
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+}
